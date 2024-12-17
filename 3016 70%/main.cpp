ab7aafafb9edaccb3923a7d821d61835322a141d
Michael Ajayi <44640349+MichaelAjayi01@users.noreply.github.com>
2024-12-17 16:49:43 +0000

    irrklang library intalled

--- 3016 70%/main.cpp
+++ 3016 70%/main.cpp
@@ -8,6 +8,7 @@
 #include <glm.hpp>
 #include <gtc/matrix_transform.hpp>  // For transformations
 #include <gtc/type_ptr.hpp>  // For glm::value_ptr
+#include <irrKlang.h>
 
 int main() {
     std::cout << "All includes are successfully found!" << std::endl;
